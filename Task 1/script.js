c3ff6cf408a32405a615b99c8178a4e14ae9cdf7
donisdonatas <donisdonatas@gmail.com>
2022-11-15 18:36:42 +0200

    Egzamino sprendiniai

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,6 +1,6 @@
 /* ------------------------------ TASK 1 ----------------------------
 Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
-pamatyti jo pateikto svorio kovertavimą į:
+pamatyti jo pateikto svorio konvertavimą į:
 1. Svarus (lb) | Formulė: lb = kg * 2.2046
 2. Gramus (g) | Formulė: g = kg / 0.0010000
 3. Uncijos (oz) | Formulė: oz = kg * 35.274
@@ -8,3 +8,17 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click', convertWeight);
+
+function convertWeight() {
+    let weight_kg = document.getElementById('search').value;
+    let weight_lb = (weight_kg * 2.2046).toFixed(3);
+    let weight_gr = (weight_kg / 0.001).toFixed(0);
+    let weight_oz = (weight_kg * 35.274).toFixed(3);
+
+    let output = `<h2>Your weight in:</h2><br>Pounds (lb) - ${weight_lb};<br>Grams (g) - ${weight_gr};<br>Ounces (oz) - ${weight_oz}`;
+    document.getElementById('output').innerHTML = output;
+}
